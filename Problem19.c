7e16d69c34dbe38c58824118da646fa5bc690c9b
itsnotTalha <talhajubayer737@gmail.com>
2024-08-06 18:40:10 +0600

    Idk this too

---
+++ Problem19.c
@@ -0,0 +1,14 @@
+/******************************************* 
+WAP that will calculate following mathematical function for the input of x. Use only the
+series to solve the problem.
+sinx=x-(x^3/3!)+(x^5/5!)-(x^7/7!)+....(inf)
+********************************************/
+#include<stdio.h>
+int main(){
+    printf("Hello");
+}
+/******************************************* 
+ * Author- Talha Jubayer
+ * talhajubayer737@gmail.com
+ * BSCSE@United International University
+********************************************/
