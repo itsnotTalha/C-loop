3994dbed3c302e78cf3a920f76462e6c85bf4230
itsnotTalha <talhajubayer737@gmail.com>
2024-08-06 18:10:12 +0600

    I dont know how to do it.

---
+++ Problem16.c
@@ -0,0 +1,16 @@
+/******************************************* 
+WAP that will find the GCD (greatest common divisor) and LCM (least common multiple)
+of two positive integers.
+********************************************/
+#include<stdio.h>
+int main(){
+    int num1,num2,gcd,lcm;
+    printf("Enter two integer number: ");
+    scanf("%d %d", &num1,&num2);
+    
+}
+/******************************************* 
+ * Author- Talha Jubayer
+ * talhajubayer737@gmail.com
+ * BSCSE@United International University
+********************************************/
